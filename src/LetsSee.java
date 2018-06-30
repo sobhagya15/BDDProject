70dedf68f2d2bbd252cbf4f2c5f4e084cbabe6c7
IBM <IBM@IBM-Testing09>
2018-06-30 10:14:28 +0530

    ashna added new sop

--- src/LetsSee.java
+++ src/LetsSee.java
@@ -4,5 +4,6 @@ public class LetsSee {
 	  System.out.println("Let's do this");
 	  System.out.println("hello dada");
 	  System.out.println("Hello Everyone");
+	  System.out.println("What is the progress? ");
   }
 }
